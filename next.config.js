06c4c2d192437eeb77222ed6dea19928766d63ac
Andrew Li <zeyu.li23@gmail.com>
2022-11-14 07:50:33 -0700

    updated cache expiry to be 1 hour

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+// set cache expiry for 1 hour
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=3600",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
